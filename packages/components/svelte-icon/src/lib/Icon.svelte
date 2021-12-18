0ee061a99040d52810ea6f879919c9638fd569aa
justinvoitel <justin.voitel@gmail.com>
2021-12-18 21:59:33 +0100

    [fix] make svelte icon theme reactive

--- packages/components/svelte-icon/src/lib/Icon.svelte
+++ packages/components/svelte-icon/src/lib/Icon.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	export let src: any;
-	export let size = '100%';
-	export let theme = 'default';
-	let customClass = '';
-	export { customClass as class };
+	export let src: any
+	export let size = '100%'
+	export let theme = 'default'
+	let customClass = ''
+	export { customClass as class }
 
-	const icon = src?.[theme] ?? src?.['default'];
+	$: icon = src?.[theme] ?? src?.['default']
 
 	if (size !== '100%') {
 		if (size.slice(-1) != 'x' && size.slice(-1) != 'm' && size.slice(-1) != '%') {
 			try {
-				size = parseInt(size) + 'px';
+				size = parseInt(size) + 'px'
 			} catch (error) {
-				size = '100%';
+				size = '100%'
 			}
 		}
 	}
