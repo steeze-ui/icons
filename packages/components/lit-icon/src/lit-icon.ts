31b3a864fad7c36f7bf91310cc9a36037f1e12e6
justinvoitel <justin.voitel@gmail.com>
2022-01-07 21:31:48 +0100

    * add lit icon with examples

--- packages/components/lit-icon/src/lit-icon.ts
+++ packages/components/lit-icon/src/lit-icon.ts
@@ -1,65 +1,56 @@
 import { html, LitElement } from 'lit'
 import { ifDefined } from 'lit/directives/if-defined.js'
-import { customElement, property, state } from 'lit/decorators.js'
-
-import { AcademicCap } from '@steeze-ui/heroicons'
+import { unsafeSVG } from 'lit/directives/unsafe-svg.js'
+import { customElement, property } from 'lit/decorators.js'
 
 @customElement('lit-icon')
 export class LitIcon extends LitElement {
+	@property({ type: Object })
+	src: any = {}
+
 	@property()
 	theme = 'default'
 
-	@property({ type: Object })
-	src: any = AcademicCap
+	@property()
+	size = '100%'
 
-	@state()
-	protected _icon: any = this.src?.[this.theme] ?? this.src?.['default']
+	@property({ attribute: 'style' })
+	styleProp = ''
 
-	connectedCallback(): void {
-		super.connectedCallback()
-		// this.style.width = '24px'
-		// this.style.height = '24px'
-		// this.style.color = 'red'
-	}
+	@property({ attribute: 'class' })
+	classProp = ''
+
+	@property({ attribute: 'aria-hidden', type: Boolean })
+	ariaHiddenProp = false
 
 	render() {
 		return html`
-			<div>
-				<svg
-					style="color:red; width:50px; height:50px;"
-					fill=${this._icon.a['fill']}
-					stroke=${ifDefined(this._icon.a['stroke'])}
-					viewBox=${this._icon.a['viewBox']}
-					xmlns=${this._icon.a['xmlns']}
-				>
-					${this._icon.p.map(
-						(e: any) => html`<path
-							stroke-linecap="${ifDefined(e['stroke-linecap'])}"
-							stroke-linejoin="${ifDefined(e['stroke-linejoin'])}"
-							stroke-width="${ifDefined(e['stroke-width'])}"
-							d=${e.d}
-						/>`
-					)}
-				</svg>
-				<svg
-					style="width:50px; height:50px; color:blue"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-					<path
-						d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-					></path>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-					></path>
-				</svg>
-			</div>
+			<svg
+				width="${this.size}"
+				height="${this.size}"
+				aria-hidden=${this.ariaHiddenProp}
+				style=${ifDefined(this.styleProp ? this.styleProp : undefined)}
+				class=${ifDefined(this.classProp ? this.classProp : undefined)}
+				fill=${ifDefined(this.src?.[this.theme]?.a?.['fill'] ?? undefined)}
+				stroke=${ifDefined(this.src?.[this.theme]?.a?.['stroke'] ?? undefined)}
+				viewBox=${ifDefined(this.src?.[this.theme]?.a?.['viewBox'] ?? undefined)}
+				xmlns=${ifDefined(this.src?.[this.theme]?.a?.['xmlns'] ?? undefined)}
+			>
+				${unsafeSVG(
+					this.src[this.theme]?.p
+						.map(
+							(e: any) =>
+								`<path ${e['fill'] ? 'fill=' + e['fill'] : ''} ${
+									e['clip-rule'] ? 'clip-rule=' + e['clip-rule'] : ''
+								} ${e['fill-rule'] ? 'fill-rule=' + e['fill-rule'] : ''} ${
+									e['stroke-linecap'] ? 'stroke-linecap=' + e['stroke-linecap'] : ''
+								} ${e['stroke-linejoin'] ? 'stroke-linejoin=' + e['stroke-linejoin'] : ''} ${
+									e['stroke-width'] ? 'stroke-width=' + e['stroke-width'] : ''
+								} d="${e.d}"></path>`
+						)
+						.join('') ?? [].join('')
+				)}
+			</svg>
 		`
 	}
 }
--- packages/components/lit-icon/vite.config.ts
+++ packages/components/lit-icon/vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
-    }
-  }
+	build: {
+		lib: {
+			entry: 'src/my-element.ts',
+			formats: ['es']
+		},
+		rollupOptions: {
+			external: /^lit/
+		}
+	},
+	optimizeDeps: {
+		include: ['@steeze-ui/heroicons', '@steeze-ui/radix-icons', '@steeze-ui/iconic-free']
+	}
 })
