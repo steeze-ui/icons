718e24dabdd803130be84c49d7b5c2d693fa490c
justinvoitel <justin.voitel@gmail.com>
2022-01-08 16:56:15 +0100

    * add octicons example to svelte-icon

--- packages/components/svelte-icon/src/routes/index.svelte
+++ packages/components/svelte-icon/src/routes/index.svelte
@@ -3,8 +3,11 @@
 	import * as HeroIcons from '@steeze-ui/heroicons'
 	import * as RadixIcons from '@steeze-ui/radix-icons'
 	import * as IconicFreeIcons from '@steeze-ui/iconic-free'
+	import * as Octicons from '@steeze-ui/octicons'
 
 	let isSolid = false
+
+	let octiconsTheme = 'default'
 </script>
 
 <div style="display:flex; flex-direction:column; gap:1rem">
@@ -42,6 +45,42 @@
 		<div style="display:flex;gap:2rem; flex-wrap:wrap">
 			{#each Object.keys(IconicFreeIcons) as key}
 				<Icon src={IconicFreeIcons[key]} size="1rem" style="color:rgb(237, 237, 238)" />
+			{/each}
+		</div>
+	</div>
+	<div>
+		<div style="display:flex;gap:2rem; align-items:center">
+			<h1 style="color:white">Octicons</h1>
+			<div style="display:flex; gap:1rem">
+				<div>
+					<input
+						type="radio"
+						id="default"
+						name="octicons-theme"
+						value="default"
+						bind:group={octiconsTheme}
+					/>
+					<label for="default" style="color:white">default(16px)</label>
+				</div>
+				<div>
+					<input type="radio" id="24" name="octicons-theme" value="24" bind:group={octiconsTheme} />
+					<label for="24" style="color:white">24px</label>
+				</div>
+				<div>
+					<input type="radio" id="12" name="octicons-theme" value="12" bind:group={octiconsTheme} />
+					<label for="12" style="color:white">12px</label>
+				</div>
+			</div>
+		</div>
+		<div style="display:flex;gap:2rem; flex-wrap:wrap">
+			{#each Object.keys(Octicons) as key}
+				<Icon
+					id={key}
+					src={Octicons[key]}
+					theme={octiconsTheme}
+					size="1rem"
+					style="color:rgb(237, 237, 238)"
+				/>
 			{/each}
 		</div>
 	</div>
