75a750422f7679464a6bedf914860d53c8d23523
justinvoitel <mail@justinv.me>
2022-02-08 13:23:00 +0100

    chore(svelte-icon): improve example page

--- packages/components/svelte-icon/src/routes/index.svelte
+++ packages/components/svelte-icon/src/routes/index.svelte
@@ -5,94 +5,79 @@
 	import * as IconicFreeIcons from '@steeze-ui/iconic-free'
 	import * as Octicons from '@steeze-ui/octicons'
 	import * as FeatherIcons from '@steeze-ui/feather-icons'
+	import * as RemixIcons from '@steeze-ui/remix-icons'
 
 	let isSolid = false
 
 	let octiconsTheme = 'default'
+
+	const iconpacks = {
+		Heroicons: {
+			icons: HeroIcons,
+			themes: ['default', 'solid'],
+			activeTheme: 'default'
+		},
+		RadixIcons: {
+			icons: RadixIcons,
+			themes: ['default'],
+			activeTheme: 'default'
+		},
+		IconicFreeIcons: {
+			icons: IconicFreeIcons,
+			themes: ['default'],
+			activeTheme: 'default'
+		},
+		Octicons: {
+			icons: Octicons,
+			themes: ['default', '24', '12'],
+			activeTheme: 'default'
+		},
+		FeatherIcons: {
+			icons: FeatherIcons,
+			themes: ['default'],
+			activeTheme: 'default'
+		},
+		RemixIcons: {
+			icons: RemixIcons,
+			themes: ['default', 'solid'],
+			activeTheme: 'default'
+		}
+	}
 </script>
 
 <div style="display:flex; flex-direction:column; gap:1rem">
-	<div>
-		<div style="display:flex;gap:2rem; align-items:center">
-			<h1 style="color:white">Heroicons</h1>
-			<div>
-				<input id="solid-toggle" type="checkbox" bind:checked={isSolid} />
-				<label for="solid-toggle" style="color:white">solid</label>
+	{#each Object.keys(iconpacks) as iconpack}
+		<div>
+			<div style="display:flex;gap:2rem; align-items:center">
+				<h1 style="color:white">{iconpack}</h1>
+				<div style="display:flex; gap:1rem">
+					{#each iconpacks[iconpack].themes as theme}
+						{@const checked = theme === iconpacks[iconpack].activeTheme ? true : null}
+						<div>
+							<input
+								type="radio"
+								id={theme}
+								name="{iconpack}-theme"
+								value={theme}
+								{checked}
+								on:change={(e) => (iconpacks[iconpack].activeTheme = theme)}
+							/>
+							<label for={theme} style="color:white">{theme}</label>
+						</div>
+					{/each}
+				</div>
 			</div>
-		</div>
-		<div style="display:flex;gap:2rem; flex-wrap:wrap">
-			{#each Object.keys(HeroIcons) as key}
-				<Icon
-					src={HeroIcons[key]}
-					theme={isSolid ? 'solid' : 'default'}
-					size="1rem"
-					style="color:rgb(237, 237, 238)"
-				/>
-			{/each}
-		</div>
-	</div>
-
-	<div>
-		<h1 style="color:white">Radix Icons</h1>
-		<div style="display:flex;gap:2rem; flex-wrap:wrap">
-			{#each Object.keys(RadixIcons) as key}
-				<Icon src={RadixIcons[key]} size="1rem" style="color:rgb(237, 237, 238)" />
-			{/each}
-		</div>
-	</div>
-
-	<div>
-		<h1 style="color:white">Iconic Free Icons</h1>
-		<div style="display:flex;gap:2rem; flex-wrap:wrap">
-			{#each Object.keys(IconicFreeIcons) as key}
-				<Icon src={IconicFreeIcons[key]} size="1rem" style="color:rgb(237, 237, 238)" />
-			{/each}
-		</div>
-	</div>
-
-	<div>
-		<div style="display:flex;gap:2rem; align-items:center">
-			<h1 style="color:white">Octicons</h1>
-			<div style="display:flex; gap:1rem">
-				<div>
-					<input
-						type="radio"
-						id="default"
-						name="octicons-theme"
-						value="default"
-						bind:group={octiconsTheme}
+			<div style="display:flex;gap:2rem; flex-wrap:wrap">
+				{#each Object.keys(iconpacks[iconpack].icons) as key}
+					<Icon
+						id={key}
+						src={iconpacks[iconpack].icons[key]}
+						theme={iconpacks[iconpack].activeTheme}
+						size="1rem"
+						style="color:rgb(237, 237, 238)"
 					/>
-					<label for="default" style="color:white">default(16px)</label>
-				</div>
-				<div>
-					<input type="radio" id="24" name="octicons-theme" value="24" bind:group={octiconsTheme} />
-					<label for="24" style="color:white">24px</label>
-				</div>
-				<div>
-					<input type="radio" id="12" name="octicons-theme" value="12" bind:group={octiconsTheme} />
-					<label for="12" style="color:white">12px</label>
-				</div>
+				{/each}
 			</div>
 		</div>
-		<div style="display:flex;gap:2rem; flex-wrap:wrap">
-			{#each Object.keys(Octicons) as key}
-				<Icon
-					id={key}
-					src={Octicons[key]}
-					theme={octiconsTheme}
-					size="1rem"
-					style="color:rgb(237, 237, 238)"
-				/>
-			{/each}
-		</div>
-	</div>
-
-	<div>
-		<h1 style="color:white">Feather Icons</h1>
-		<div style="display:flex;gap:2rem; flex-wrap:wrap">
-			{#each Object.keys(FeatherIcons) as key}
-				<Icon src={FeatherIcons[key]} size="1rem" style="color:rgb(237, 237, 238)" />
-			{/each}
-		</div>
-	</div>
+	{/each}
 </div>
