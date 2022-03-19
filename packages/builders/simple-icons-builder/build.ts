460a191cbbf4e737792df6012bbc5281a3aa795e
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2022-03-19 11:40:47 +0100

    simple-icons(build): implement build script

--- packages/builders/simple-icons-builder/build.ts
+++ packages/builders/simple-icons-builder/build.ts
@@ -1,16 +1,33 @@
 import { ThemeBuilder } from '@steeze-ui/icons'
+import fs from 'fs/promises'
+import path from 'path'
 
-const builder = new ThemeBuilder({
-	sources: {
-		// inputRaw: './node_modules/', //* use this if your sources come from a npm package
-		// collectFromDir: { icons: 'default' }, //* use this if icon themes are bound to the overlying derectory (e.g. outline/cake.svg, fill/cake.svg).
-		// collectFromSuffix: { '-line': 'default', fill: 'solid' }, //* use this if icon themes are bound to a suffix (e.g. cake-line.svg, cake-fill.svg).
-		// fallbackTheme: 'default', //* collect in which theme folder, when icons cannot be identified by their suffix (default is dont collect)
-	},
-	lib: {
-		excludeSvgAttributes: ['xmlns', 'width', 'height', 'class'], //* exclude certain attributes from the svg's, which might be controlled by the used Icon component
-		extendSvgAttributes: {} //* extend the svg's attributes
-	}
+async function prepare(inputDir: string, outputDir: string) {
+	const files = await fs.readdir(inputDir)
+
+	await fs.rmdir(outputDir, { recursive: true })
+	await fs.mkdir(outputDir, { recursive: true })
+
+	return Promise.all(
+		files.map(async (file) => {
+			if (path.extname(file) !== '.svg') {
+				return
+			}
+
+			await fs.copyFile(path.join(inputDir, file), path.join(outputDir, file))
+
+			console.log(`Copied ${file}`)
+		})
+	)
+}
+
+prepare('./node_modules/simple-icons/icons', './themes/default').then(() => {
+	const builder = new ThemeBuilder({
+		sources: {},
+		lib: {
+			excludeSvgAttributes: ['xmlns']
+		}
+	})
+
+	builder.build()
 })
-
-builder.build() //* if neither collectFromDir or collectFromSuffix are defined, a already populated themes directory with at least a default theme is necessary (see example in this themplate)
