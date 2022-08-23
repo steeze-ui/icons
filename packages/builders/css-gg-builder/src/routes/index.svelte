8063ca5bc4e6070befff76733d2df33b33b86f64
Yash <yash@samitsolutions.com>
2022-08-23 02:00:13 -0400

    css.gg support added

---
+++ packages/builders/css-gg-builder/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Icon } from '@steeze-ui/svelte-icon'
+	import { Add } from '$lib'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Icon src={Add} size="100" />
